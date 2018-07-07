d6aca07fce2f3cb76e9ecaece68ffc451de54305
Danny G Smith <dgs@campfirepixels.com>
2018-07-07 10:24:15 -0500

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 'use strict';
 // Require our dependencies
 
-// const dotenv = require('dotenv').config();
-const autoprefixer = require('autoprefixer');
-const babel = require('gulp-babel');
-const concat = require('gulp-concat');
-const del = require('del');
-const eslint = require('gulp-eslint');
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const notify = require('gulp-notify');
-const pleeease = require('gulp-pleeease');
-const plumber = require('gulp-plumber');
-const preprocess = require('gulp-preprocess');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
-const stripDebug = require('gulp-strip-debug');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
+const autoprefixer = require( 'autoprefixer' );
+const babel = require( 'gulp-babel' );
+const concat = require( 'gulp-concat' );
+const del = require( 'del' );
+const {src, task} = require('gulp');
+const eslint = require( 'gulp-eslint' );
+const gulp = require( 'gulp' );
+const gutil = require( 'gulp-util' );
+const notify = require( 'gulp-notify' );
+const pleeease = require( 'gulp-pleeease' );
+const plumber = require( 'gulp-plumber' );
+const preprocess = require( 'gulp-preprocess' );
+const rename = require( 'gulp-rename' );
+const sass = require( 'gulp-sass' );
+const sassLint = require( 'gulp-sass-lint' );
+const stripDebug = require( 'gulp-strip-debug' );
+const sourcemaps = require( 'gulp-sourcemaps' );
+const uglify = require( 'gulp-uglify' );
+const browserSync = require( 'browser-sync' ).create();
 const reload = browserSync.reload;
 const destFolder = 'dist/';
-const pkg = require('./package.json');
-const devBuild = ((process.env.NODE_ENV || 'development')
-   .trim().toLocaleLowerCase() !== 'production');
+const pkg = require( './package.json' );
+const devBuild = ( ( process.env.NODE_ENV || 'development' )
+   .trim().toLocaleLowerCase() !== 'production' );
 
 
 // Set assets paths.
@@ -32,10 +33,10 @@ const devBuild = ((process.env.NODE_ENV || 'development')
  *
  */
 const paths = {
-   'css': ['app/css/**/*.css'],
-   'js': ['app/js/**/*.js'],
-   'scss': ['app/scss/**/*.scss'],
-   'tests': ['tests/spec/**/*.js'],
+   'css': [ 'app/css/**/*.css' ],
+   'js': [ 'app/js/**/*.js' ],
+   'scss': [ 'app/scss/**/*.scss' ],
+   'tests': [ 'tests/spec/**/*.js' ],
 };
 
 /**
@@ -43,8 +44,8 @@ const paths = {
  *
  */
 const dist = {
-   'css': ['./dist/css/'],
-   'js': ['./dist/js/'],
+   'css': [ './dist/css/' ],
+   'js': [ './dist/js/' ],
 };
 
 
@@ -53,8 +54,8 @@ const dist = {
  *
  */
 const html = {
-   in: ['app/**/*.html'],
-   out: ['./dist/'],
+   in: [ 'app/**/*.html' ],
+   out: [ './dist/' ],
    context: {
       devBuild: devBuild,
       author: pkg.author,
@@ -69,8 +70,8 @@ const html = {
 const pleeeaseOpts = {
    'in': 'app/scss/style.css',
    'out': 'dist/css/style.css',
-   'autoprefixer': {'browsers': ['last 2 versions', '> 2%']},
-   'rem': ['18px'],
+   'autoprefixer': { 'browsers': [ 'last 2 versions', '> 2%' ] },
+   'rem': [ '18px' ],
    'pseudoElements': true,
    'mqpacker': true,
    'minifier': !devBuild,
@@ -86,72 +87,72 @@ const jasmine = {
  * Handle errors and alert the user.
  */
 function handleErrors() {
-   const args = Array.prototype.slice.call(arguments);
+   const args = Array.prototype.slice.call( arguments );
 
-   notify.onError({
-      'title': 'Task Failed [<%= error.message %>',
-      'message': 'See console.',
-      'sound': 'Sosumi', // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
-   }).apply(this, args);
+   notify.onError( {
+                      'title': 'Task Failed [<%= error.message %>',
+                      'message': 'See console.',
+                      'sound': 'Sosumi', // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+                   } ).apply( this, args );
 
    gutil.beep(); // Beep 'sosumi' again.
 
    // Prevent the 'watch' task from stopping.
-   this.emit('end');
+   this.emit( 'end' );
 }
 
 // build HTML files
 /**
  *
  */
-gulp.task('html', function() {
-   return gulp.src(html.in)
-   // Deal with errors.
-      .pipe(plumber({'errorHandler': handleErrors}))
-      .pipe(preprocess({context: html.context})) // pass object with temp vars
-      .pipe(gulp.dest(html.out));
-});
+gulp.task( 'html', function() {
+   return gulp.src( html.in )
+              // Deal with errors.
+              .pipe( plumber( { 'errorHandler': handleErrors } ) )
+              .pipe( preprocess( { context: html.context } ) ) // pass object with temp vars
+              .pipe( gulp.dest( html.out ) );
+} );
 
 
 /**
  *
  */
 // compile Sass
-gulp.task('styles', function(done) {
-   del([dist.css + 'style.css', dist.css + 'style.min.css']);
-   gulp.src(paths.scss)
-   // Deal with errors.
-      .pipe(plumber(
-         {'errorHandler': handleErrors}
-      ))
-      .pipe(pleeease())
-      .pipe(rename({
-         extname: '.css',
-      }))
-      .pipe(gulp.dest(dist.css))
-      .pipe(rename({
-         suffix: '.min',
-         extname: '.css',
-      }))
-      .pipe(gulp.dest(dist.css))
-      .pipe(browserSync.reload({stream: true}));
+gulp.task( 'styles', function( done ) {
+   del( [ dist.css + 'style.css', dist.css + 'style.min.css' ] );
+   gulp.src( paths.scss )
+       // Deal with errors.
+       .pipe( plumber(
+          { 'errorHandler': handleErrors }
+       ) )
+       .pipe( pleeease() )
+       .pipe( rename( {
+                         extname: '.css',
+                      } ) )
+       .pipe( gulp.dest( dist.css ) )
+       .pipe( rename( {
+                         suffix: '.min',
+                         extname: '.css',
+                      } ) )
+       .pipe( gulp.dest( dist.css ) )
+       .pipe( browserSync.reload( { stream: true } ) );
    done();
-});
+} );
 
 
 /**
  *
  */
-gulp.task('maps', function(done) {
-   gulp.src(paths.scss, {base: '.'})
-   // Deal with errors.
-      .pipe(plumber({'errorHandler': handleErrors}))
-      .pipe(sourcemaps.init())
-      .pipe(pleeease())
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./dist'));
+gulp.task( 'maps', function( done ) {
+   gulp.src( paths.scss, { base: '.' } )
+       // Deal with errors.
+       .pipe( plumber( { 'errorHandler': handleErrors } ) )
+       .pipe( sourcemaps.init() )
+       .pipe( pleeease() )
+       .pipe( sourcemaps.write() )
+       .pipe( gulp.dest( './dist' ) );
    done();
-});
+} );
 
 /**
  * Concatenate and transform JavaScript.
@@ -159,22 +160,22 @@ gulp.task('maps', function(done) {
  * https://www.npmjs.com/package/gulp-concat
  * https://github.com/babel/gulp-babel
  */
-gulp.task('concat', () =>
-   gulp.src(paths.js)
+gulp.task( 'concat', () =>
+   gulp.src( paths.js )
 
-   // Deal with errors.
-      .pipe(plumber({'errorHandler': handleErrors}))
+       // Deal with errors.
+       .pipe( plumber( { 'errorHandler': handleErrors } ) )
 
-      // Convert ES6+ to ES2015.
-      .pipe(babel({
-         presets: ['ES2015'],
-      }))
+       // Convert ES6+ to ES2015.
+       .pipe( babel( {
+                        presets: [ 'ES2015' ],
+                     } ) )
 
-      // Concatenate partials into a single script.
-      .pipe(concat('app.js'))
+       // Concatenate partials into a single script.
+       .pipe( concat( 'app.js' ) )
 
-      // Save the file.
-      .pipe(gulp.dest(destFolder))
+       // Save the file.
+       .pipe( gulp.dest( destFolder ) )
 );
 
 /**
@@ -182,87 +183,99 @@ gulp.task('concat', () =>
  *
  * https://www.npmjs.com/package/gulp-uglify
  */ // gulp.series('concat'),
-gulp.task('scripts', function(done) {
-   if (devBuild) {
-      gulp.src(paths.js)
-         .pipe(plumber({'errorHandler': handleErrors}))
+gulp.task( 'scripts', function( done ) {
+   if ( devBuild ) {
+      gulp.src( paths.js )
+          .pipe( plumber( { 'errorHandler': handleErrors } ) )
 
-         // Convert ES6+ to ES2015.
-         .pipe(babel({
-            presets: ['ES2015'],
-         }))
-         .pipe(gulp.dest(dist.js));
+          // Convert ES6+ to ES2015.
+          .pipe( babel( {
+                           presets: [ 'ES2015' ],
+                        } ) )
+          .pipe( gulp.dest( dist.js ) );
    } else {
-      gulp.src(paths.js)
-         .pipe(concat('app.js'))
-         .pipe(plumber({'errorHandler': handleErrors}))
+      gulp.src( paths.js )
+          .pipe( concat( 'app.js' ) )
+          .pipe( plumber( { 'errorHandler': handleErrors } ) )
 
-         // Convert ES6+ to ES2015.
-         .pipe(babel({
-            presets: ['ES2015'],
-         }))
-         .pipe(stripDebug())
-         .pipe(uglify())
-         .pipe(rename({'suffix': '.min'}))
-         .pipe(gulp.dest(dist.js));
+          // Convert ES6+ to ES2015.
+          .pipe( babel( {
+                           presets: [ 'ES2015' ],
+                        } ) )
+          .pipe( stripDebug() )
+          .pipe( uglify() )
+          .pipe( rename( { 'suffix': '.min' } ) )
+          .pipe( gulp.dest( dist.js ) );
    }
    done();
-});
+} );
+
+/**
+ * Sass linting.
+ *
+ * https://www.npmjs.com/package/sass-lint
+ */
+gulp.task( 'sass:lint', () =>
+   gulp.src( paths.scss )
+       .pipe( sassLint() )
+       .pipe( sassLint.format() )
+       .pipe( sassLint.failOnError() )
+);
 
 /**
  * JavaScript linting.
  *
  * https://www.npmjs.com/package/gulp-eslint
  */
-gulp.task('js:lint', () =>
-   gulp.src(paths.js)
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError())
-);
-
-gulp.task('browser-sync', function() {
-   browserSync.init({
-      server: {
-         serveStaticOptions: {
-            extensions: ['html'],
-         },
-         baseDir: 'app/',
-         index: 'index.html',
-      },
-   });
+gulp.task( 'js:lint', () => {
+   gulp.src( paths.js )
+      .pipe( eslint() )
+      .pipe( eslint.format() )
+      .pipe( eslint.failAfterError() )
 });
+
+gulp.task( 'browser-sync', function() {
+   browserSync.init( {
+                        server: {
+                           serveStaticOptions: {
+                              extensions: [ 'html' ],
+                           },
+                           baseDir: 'app/',
+                           index: 'index.html',
+                        },
+                     } );
+} );
 
 /**
  * Process tasks and reload browsers on file changes.
  *
  * https://www.npmjs.com/package/browser-sync
  */
-gulp.task('watch', function() {
-   browserSync.init({
-      server: {
-         serveStaticOptions: {
-            extensions: ['html'],
-         },
-         baseDir: 'app/',
-         index: 'index.html',
-      },
-   });
+gulp.task( 'watch', function() {
+   browserSync.init( {
+                        server: {
+                           serveStaticOptions: {
+                              extensions: [ 'html' ],
+                           },
+                           baseDir: 'app/',
+                           index: 'index.html',
+                        },
+                     } );
 
    // Run tasks when files change.
-   gulp.watch(html.in, gulp.series('html', browserSync.reload));
-   gulp.watch(paths.css, gulp.series('styles'));
-   gulp.watch(paths.js, gulp.series('scripts', browserSync.reload));
-   //gulp.watch(paths.js, gulp.series('js:lint'));
-});
+   gulp.watch( html.in, gulp.series( 'html', browserSync.reload ) );
+   gulp.watch( paths.css, gulp.series( 'styles' ) );
+   gulp.watch( paths.js, gulp.series( 'scripts', browserSync.reload ) );
+   gulp.watch( paths.js, gulp.series( 'js:lint' ) );
+} );
 
 /**
  * Create individual tasks.
  */
-gulp.task('browser-sync', gulp.series('browser-sync'));
-gulp.task('html', gulp.series('html'));
-gulp.task('scripts', gulp.series('scripts'));
-gulp.task('styles', gulp.series('styles'));
-gulp.task('maps', gulp.series('maps'));
-gulp.task('lint', gulp.series('js:lint'));
+gulp.task( 'browser-sync', gulp.series( 'browser-sync' ) );
+gulp.task( 'html', gulp.series( 'html' ) );
+gulp.task( 'scripts', gulp.series( 'scripts' ) );
+gulp.task( 'styles', gulp.series( 'styles' ) );
+gulp.task( 'maps', gulp.series( 'maps' ) );
+gulp.task( 'lint', gulp.series( 'js:lint' ) );
 //gulp.task('default', gulp.series('html', 'styles', 'scripts'));
